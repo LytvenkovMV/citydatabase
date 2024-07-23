f790f6feda93a83826ae1018fb9f7375979154fb
Home PC <example@mail.com>
2024-07-23 23:01:08 +0300

    Initial commit

---
+++ src/main/java/org/example/citydatabase/CitydatabaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.citydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitydatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitydatabaseApplication.class, args);
+    }
+
+}
