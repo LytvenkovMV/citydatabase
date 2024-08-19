751c25fd2814ab997d144fd2592c1accbffba8ab
Work PC <example@mail.com>
2024-08-19 16:19:37 +0300

    Удалил файлы, которые были скопированы в новый модуль

--- src/main/java/org/example/citydatabase/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.citydatabase.repository;
-
-import org.example.citydatabase.entity.Car;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends CrudRepository<Car, Long> {
-}
--- src/main/java/org/example/citydatabase/repository/PassportRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.citydatabase.repository;
-
-import org.example.citydatabase.entity.Passport;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassportRepository extends CrudRepository<Passport, Long> {
-}
