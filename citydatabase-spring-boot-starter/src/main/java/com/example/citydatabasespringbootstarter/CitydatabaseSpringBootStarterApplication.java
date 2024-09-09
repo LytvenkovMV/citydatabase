305dd89c3803821e7e3b82d1d0472723ff5571ff
Work PC <lytvenkovmv@gmail.com>
2024-09-09 15:45:11 +0300

    Initial commit

---
+++ citydatabase-spring-boot-starter/src/main/java/com/example/citydatabasespringbootstarter/CitydatabaseSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.citydatabasespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitydatabaseSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitydatabaseSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ citydatabase-spring-boot-starter/src/test/java/com/example/citydatabasespringbootstarter/CitydatabaseSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citydatabasespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitydatabaseSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
