8f5e8aecfa44c120183352b3cf7a66401ef28dec
Home PC <example@mail.com>
2024-09-11 21:53:06 +0300

    Исправил ошибки

--- bankServer/src/main/java/com/example/bankserver/kafka/impl/PersonsListeningServiceImpl.java
+++ bankServer/src/main/java/com/example/bankserver/kafka/impl/PersonsListeningServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.bankserver.kafka.impl;
 
 import com.example.bankserver.kafka.PersonsListeningService;
 import com.example.bankserver.service.AccountService;
+import com.example.citydatabasespringbootstarter.service.MessageListeningService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,6 @@ public class PersonsListeningServiceImpl implements PersonsListeningService {
     @Override
     public void listen(Long[] personIds) {
 
-        accountService.addAccountList(service.listenLongArray());
+        accountService.addAccountList(service.listenLongArray(new Long[100]));
     }
 }
