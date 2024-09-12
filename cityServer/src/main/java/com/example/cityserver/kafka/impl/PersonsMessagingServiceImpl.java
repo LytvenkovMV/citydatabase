a30cc8dce0bd8d08de697a58b396273d68524e84
Work PC <lytvenkovmv@gmail.com>
2024-09-12 11:07:21 +0300

    Добавил сервис для отправки сообщений из кастомного стартера

--- cityServer/src/main/java/com/example/cityserver/kafka/impl/PersonsMessagingServiceImpl.java
+++ cityServer/src/main/java/com/example/cityserver/kafka/impl/PersonsMessagingServiceImpl.java
@@ -1,19 +1,19 @@
 package com.example.cityserver.kafka.impl;
 
+import com.example.citydatabasespringbootstarter.service.MessageSendingService;
 import com.example.cityserver.kafka.PersonsMessagingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PersonsMessagingServiceImpl implements PersonsMessagingService {
 
-    private final KafkaTemplate<String, Long[]> kafkaTemplate;
+    private final MessageSendingService service;
 
     @Override
     public void sendPersons(Long[] personIds) {
 
-        kafkaTemplate.send("newpersons", personIds);
+        service.sendLongArray(personIds);
     }
 }
