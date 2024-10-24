3eb6bd89cb759cc7a577221e8839c2fcaa3a62f0
Work PC <lytvenkovmv@gmail.com>
2024-10-24 15:08:16 +0300

    Добавил методы для обработки исключений при валидации в Controller Advice

--- cityServer/src/main/java/com/example/cityserver/controller/ExControllerAdvice.java
+++ cityServer/src/main/java/com/example/cityserver/controller/ExControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.example.cityserver.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,14 +12,28 @@ import java.util.NoSuchElementException;
 public class ExControllerAdvice {
 
     @ExceptionHandler(value = {NoSuchElementException.class})
-    public ResponseEntity<String> handleBadRequestEx(NoSuchElementException e) {
+    public ResponseEntity<String> handleNoSuchElementEx(NoSuchElementException e) {
+        e.printStackTrace();
+
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public ResponseEntity<String> handleConstraintViolationEx(ConstraintViolationException e) {
+        e.printStackTrace();
+
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<String> handleMethodArgumentNotValidEx(MethodArgumentNotValidException e) {
         e.printStackTrace();
 
         return ResponseEntity.status(404).body(e.getMessage());
     }
 
     @ExceptionHandler(value = {RuntimeException.class})
-    public ResponseEntity<String> handleBadRequestEx(RuntimeException e) {
+    public ResponseEntity<String> handleRuntimeEx(RuntimeException e) {
         e.printStackTrace();
 
         return ResponseEntity.status(503).body("Service Unavailable");
